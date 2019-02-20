a0426501fb8ea29ba2d6c6d8918ee37d1707e624
YMM <cinyc.s@gmail.com>
2019-02-20 07:08:24 +0200

    Add more tests

--- SimpleTests/UnitTest1.cs
+++ SimpleTests/UnitTest1.cs
@@ -11,11 +11,27 @@ namespace Tests
         }
 
         [Test]
-        public void Test1()
+        public void SimpleReversalWorks()
         {
             Program p = new Program();
             string result = p.Reverse("MooCow");
             Assert.AreEqual("woCooM", result);
+        }
+
+        [Test]
+        public void ReversalOfEmptyStringWorks()
+        {
+            Program p = new Program();
+            string result = p.Reverse("");
+            Assert.AreEqual("", result);
+        }
+
+        [Test]
+        public void ReversalOfNullWorks()
+        {
+            Program p = new Program();
+            string result = p.Reverse(null);
+            Assert.AreEqual(null, result);
         }
     }
 }
